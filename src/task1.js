28313c8075a84dd267a67e62283120812a616e07
Krasulin <r.krasulin26@gmail.com>
2025-10-27 10:57:44 +0300

    1 кт

--- src/task1.js
+++ src/task1.js
@@ -7,9 +7,3 @@ function multipliedByN(initialArray, multiplier) {
   
   return newArray;
 }
-
-const initial = [1, 3, 5];
-const result = multipliedByN(initial, -2);
-
-console.log(initial); // [1, 3, 5]
-console.log(result);  // [-2, -6, -10]
