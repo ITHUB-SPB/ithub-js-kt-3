9ed369d0bb7f63ebdd515ed2e37f421c38423eab
Konstantin Barbarich <barbarickosntantin@gmail.com>
2025-12-14 21:03:00 +0300

    done

--- src/task2.js
+++ src/task2.js
@@ -1,11 +1,29 @@
 /**
  * @param {string[]} values - Массив строк
  * @returns {number} Сумма по численным данным
- * 
- * @description Подсчет суммы элементов массива, 
+ *
+ * @description Подсчет суммы элементов массива,
  * корректно переводимых из строки в численный вид.
  */
-export function calculateSum(values) {
 
-  return 0
+export function calculateSum(values) {
+  if (values.length === 0) {
+    throw new Error("Передан пустой массив");
+  }
+  let sum = 0;
+  let NunbersIsValid = false;
+
+  for (const value of values) {
+    const num = +value;
+    if (!isNaN(num)) {
+      sum += num;
+      NunbersIsValid = true;
+    }
+  }
+
+  if (!NunbersIsValid) {
+    throw new Error("Отсутствуют численные данные");
+  }
+
+  return sum;
 }
--- src/task3.js
+++ src/task3.js
@@ -1,9 +1,11 @@
 /**
  * @param {any[]} initialArray - Исходный массив
- * @returns {undefined} 
- * 
+ * @returns {undefined}
+ *
  * @description Разворот массива на месте с выводом в консоль
  */
 export function reverseArray(initialArray) {
-    return
+  initialArray.reverse();
+  console.log(initialArray);
+  return initialArray;
 }
