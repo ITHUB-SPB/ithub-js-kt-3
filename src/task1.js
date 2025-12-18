e7194b6ca68ede5911a7a9375add42a6558c524b
Ковалев Роман <r0m4.k0v@gmail.com>
2025-12-18 09:29:39 +0300

    1

--- src/task1.js
+++ src/task1.js
@@ -1,13 +1,22 @@
 /**
  * @param {number[]} initialArray - Исходный массив
  * @param {number} multiplier - Множитель
- * 
+ *
  * @returns {number[]} Новый массив
- * 
- * @description Возвращает массив с элементами, полученными 
+ *
+ * @description Возвращает массив с элементами, полученными
  * перемножением всех элементов исходного массива на число
  */
 export function multipliedByN(initialArray, multiplier) {
-
   return null;
+}
+
+export function multipliedByN(initialArray, multiplier) {
+  const multipliedArray = [];
+
+  for (let i = 0; i < initialArray.length; i++) {
+    multipliedArray.push(initialArray[i] * multiplier);
+  }
+
+  return multipliedArray;
 }
